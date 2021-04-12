afec290d1e57036a16a29fb85ad4e56662f0015f
Jesse <jesvis@bitconsult.be>
2021-04-12 17:01:14 +0200

    init

--- Classes_oef/Program.cs
+++ Classes_oef/Program.cs
@@ -6,7 +6,7 @@ namespace Classes_oef
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Worldje!");
         }
     }
 }
